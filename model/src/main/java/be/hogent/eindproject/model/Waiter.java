f655524b615d431956c6caebf2f77239d38a0200
andylandtsheer <45149851+andylandtsheer@users.noreply.github.com>
2020-02-04 23:21:13 +0100

    Mappers done and tested (#3)

--- model/src/main/java/be/hogent/eindproject/model/Waiter.java
+++ model/src/main/java/be/hogent/eindproject/model/Waiter.java
@@ -3,6 +3,8 @@ package be.hogent.eindproject.model;
 
 import lombok.Getter;
 
+import java.util.Objects;
+
 @Getter
 public class Waiter {
     private final int id;
@@ -15,5 +17,18 @@ public class Waiter {
         this.lastName = lastName;
         this.firstName = firstName;
         this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Waiter waiter = (Waiter) o;
+        return id == waiter.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
